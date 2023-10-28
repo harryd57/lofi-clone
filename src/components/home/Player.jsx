9c89e3e945566cbef4c7efdd5c6ee0687304fe26
harryd57 <harryd57@yahoo.com>
2023-10-28 05:59:43 +0100

    audio bug fixed

--- src/components/home/Player.jsx
+++ src/components/home/Player.jsx
@@ -1,18 +1,22 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactPlayer from 'react-player';
 import Day from '../../assets/day.mp4';
 import Rain from '../../assets/day-rain.mp4';
 import Rainfall from '../../assets/rainfall.mp3';
 
+let audio = new Audio(Rainfall);
+
 const Player = () => {
   const [toggle, setToggle] = useState(false);
-  let audio = new Audio(Rainfall);
 
-  const handleToggle = () =>{
-    setToggle(!toggle);
+  const handlePlay = () =>{
     toggle ? audio.play() : audio.pause();
     audio.loop = true;
   }
+
+  useEffect(()=>{
+    handlePlay();
+  }, [toggle])
 
   return (
     <section className='h-screen max-w-[1280px] mx-auto'>
@@ -28,12 +32,7 @@ const Player = () => {
       </div>
 
       <button className='ml-2 text-sm border-2 border-[#F3A592] py-2 px-6 rounded-lg'
-      onClick={()=> handleToggle()}
-      >
-        Click me
-      </button>
-      <button className='ml-2 text-sm border-2 border-[#F3A592] py-2 px-6 rounded-lg'
-      onClick={()=> audio.pause()}
+      onClick={()=> setToggle(!toggle)}
       >
         Click me
       </button>
